203ac1dbd90648d034c48508075db0908a8b1185
Kareem Al Saady <kareem.alsaady@gmail.com>
2018-03-26 11:32:02 +0400

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+  import { Component } from '@angular/core';
+
+  @Component({
+    selector: 'my-app',
+    template: '<h1>Angular Skeleton</h1>'
+  })
+
+  export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+
+  //Import Modules
+  import { NgModule } from "@angular/core";
+  import { BrowserModule } from "@angular/platform-browser";
+
+  //Import Components
+  import { AppComponent } from "./app.component";
+
+  @NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+  })
+
+  export class AppModule {}
